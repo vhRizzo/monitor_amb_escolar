e7e46ba606a8727f4f17665ca3630f4ff27f46c7
Victor Hugo Rizzo Moura <43479155+vhRizzo@users.noreply.github.com>
2022-10-18 01:11:30 -0300

    Update inmp441_setup.h

--- esp-idf/school_monitor/main/inmp441/inmp441_setup.h
+++ esp-idf/school_monitor/main/inmp441/inmp441_setup.h
@@ -1,3 +1,24 @@
+/*
+ * Implementation of sound energy using INMP441 sensor with Second-Order
+ * Sections IIR Filter based on Ivan Kostoski's code available at
+ * https://github.com/ikostoski/esp32-i2s-slm.
+ * 
+ * (c)2022 Victor Moura
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *    
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef INMP441_SETUP_H
 #define INMP441_SETUP_H
 
